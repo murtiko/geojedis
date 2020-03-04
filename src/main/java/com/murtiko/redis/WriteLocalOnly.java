71ad5ad170fad4ed894827b3056bb370527f8bdf
muratc <murat.cengiz@telenity.com>
2020-03-04 13:41:46 +0300

    new Write strategy and some draft tests

---
+++ src/main/java/com/murtiko/redis/WriteLocalOnly.java
@@ -0,0 +1,18 @@
+package com.murtiko.redis;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Function;
+
+import redis.clients.jedis.Jedis;
+
+public class WriteLocalOnly implements WriteStrategy {
+
+    @Override
+    public <T> T write(GeoJedisConfig config, Function<Jedis, T> function) {
+        final List<T> resp = new LinkedList<>();
+        config.getLocalPools().forEach((k, v) -> resp.add(JedisUtil.tryExec(config, k, v, function)));
+        return resp.isEmpty() ? null : resp.get(0);
+    }
+
+}
